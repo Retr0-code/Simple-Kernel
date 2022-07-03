fe8062d38803610ea5d9a8754985c9a5e562c31f
Retr0-code <nikk3695@gmail.com>
2022-07-03 20:01:14 +0300

    Commented gets.asm

--- functions/gets.asm
+++ functions/gets.asm
@@ -2,41 +2,37 @@ global _gets
 
 
 section .text:
+    ; Get string function
     _gets:
         push bp
         mov bp, sp
 
 
-        mov si, 0
-        mov ah, 0
+        mov si, 0           ; Sets SI to 0 as is is a counter
+        mov ah, 0           ; Sets read key-press mode 
 
-        mov bx, [bp+6]
+        mov bx, [bp+6]      ; Writes address of buffer to BX
 
-        _read_char:
-            cmp al, 0x0d
-            je _done_gets
-            cmp si, [bp+4]
-            je _done_gets
+        _read_char:         ; Subroutine that reads input
+            cmp al, 0x0d    ; If "Enter" pressed
+            je _done_gets   ; Exits
+            cmp si, [bp+4]  ; If last character was entered
+            je _done_gets   ; Exits
             
-            int 16h
+            int 16h         ; Calls BIOS interuption to read key
 
-            mov ah, 0xe
-            int 10h
-            mov ah, 0
+            mov ah, 0xe     ; Sets console output mode
+            int 10h         ; Calls BIOS output interuption
+            mov ah, 0       ; Sets read key-press mode
 
-            mov [bx], al
-            inc bx
-            inc si
-            jmp _read_char
+            mov [bx], al    ; Writes character to buffer
+            inc bx          ; Shifts the buffer address
+            inc si          ; Incriments the counter
+            jmp _read_char  ; Reads next character
             
 
         _done_gets:
-            mov ah, 0xe
-            mov al, 0x0a
-            int 10h
-            mov al, 0x0d
-            int 10h
-            mov ah, 0
+            call _newline   ; Moves cursor to a new line
 
-            pop bp
-            ret
+            pop bp          ; Restores BP
+            ret             ; Exits function
